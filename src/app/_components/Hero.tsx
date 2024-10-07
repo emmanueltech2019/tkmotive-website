881b9ff32c14e432800ccf214d41d26699e45082
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-10-07 18:19:59 +0100

    repo updated

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -10,7 +10,7 @@ import Verification from "@/components/modal/Verification";
 import Details from "@/components/modal/Details";
 
 const Hero = () => {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true});
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
   useEffect(() => {
     if (emblaApi) {
       console.log(true); // Access API
@@ -47,42 +47,44 @@ const Hero = () => {
     closeDetails();
     setV(true);
   }
-  
+
   return (
-    <div className="bg-[--foreground-green]">
-      <div className="px-[--padding-x]">
+    <>
+      <div className="px-[--padding-x] sticky top-0 bg-[--foreground-green] z-50">
         <Navbar func={openCart} />
       </div>
-      {/* modal */}
-      <Cart func={closeCart} modalState={showCart} handleV={openV} />
-      <Verification func={closeV} modalState={showV} />
-      <Details func={closeDetails} modalState={showDetails} verfication={openV} />
+      <div className="bg-[--foreground-green]">
+        {/* modal */}
+        <Cart func={closeCart} modalState={showCart} handleV={openV} />
+        <Verification func={closeV} modalState={showV} />
+        <Details func={closeDetails} modalState={showDetails} verfication={openV} />
 
-      <section className="flex flex-col lg:flex-row items-center pb-16 gap-y-14 px-[--padding-x]">
-        <div
-          className={`col flex-1 lg:flex-[1.5] xl:flex-1 text-[--foreground-light-orange] ${antonFont} `}
-        >
-          <h1 className="text-[">Relax, Unwind, and Savor the Flavors</h1>
-          <p className="text-sm lg:text-6">
-            Let&apos;s Chill – Book Your Spot Now
-          </p>
-        </div>
-        <div className="col flex-1">
-          <div className="hero-image">
-            <Image src={HeroImage} alt="hero image" />
+        <section className="flex flex-col lg:flex-row items-center pb-16 gap-y-14 px-[--padding-x]">
+          <div
+            className={`col flex-1 lg:flex-[1.5] xl:flex-1 text-[--foreground-light-orange] ${antonFont} `}
+          >
+            <h1 className="text-[">Relax, Unwind, and Savor the Flavors</h1>
+            <p className="text-sm lg:text-6">
+              Let&apos;s Chill – Book Your Spot Now
+            </p>
           </div>
-        </div>
-      </section>
-      <div className="embla overflow-hidden bg-white">
-        <div className="embla__viewport" ref={emblaRef}>
-          <div className="embla__container flex gap-[50px] py-[18px] px-10">
-            {new Array(20).fill("TKMOTIVE").map((text,i) => (
-              <p key={i} className={`embla__slide text-6 ${antonFont}`}>{text}</p>
-            ))}
+          <div className="col flex-1">
+            <div className="hero-image">
+              <Image src={HeroImage} alt="hero image" />
+            </div>
+          </div>
+        </section>
+        <div className="embla overflow-hidden bg-white">
+          <div className="embla__viewport" ref={emblaRef}>
+            <div className="embla__container flex gap-[50px] py-[18px] px-10">
+              {new Array(20).fill("TKMOTIVE").map((text, i) => (
+                <p key={i} className={`embla__slide text-6 ${antonFont}`}>{text}</p>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Services from './_components/Services'
 
 const Home = () => {
   return (
-    <div>
+    <div className='relative'>
       <Hero />
       <JollofRice />
       <About />
